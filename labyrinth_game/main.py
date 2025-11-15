b1cc3f231fa8f70a000e6f70c5305d61e78b21b2
Sergey Ovsyannikov <s.ovsyannikov@gmail.com>
2025-11-15 15:30:21 +0300

    сформирована структура проекта

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+from labyrinth_game.constants import ROOMS
+from labyrinth_game.player_actions import move_player, take_item, solve_puzzle
+from labyrinth_game.utils import show_room_description, show_inventory
+
+
 def main():
     """точка входа в игру."""
     print("Первая попытка запустить проект!")
 
 if __name__ == "__main__":
     main()
+
+game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+  }
+
